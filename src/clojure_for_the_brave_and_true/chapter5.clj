b4d1e50fa7817728fe78a53d6feffed9b7ff4458
Godsmith <filip.lange@gmail.com>
2017-01-15 18:48:38 +0100

    chapter 5 1-3

--- src/clojure_for_the_brave_and_true/chapter5.clj
+++ src/clojure_for_the_brave_and_true/chapter5.clj
@@ -1,5 +1,12 @@
 (ns clojure-for-the-brave-and-true.chapter5)
 
+
+; 1. You used (comp :intelligence :attributes) to create a function that returns a character’s intelligence.
+; Create a new function, attr , that you can call like (attr :intelligence) and that does the same thing.
+(defn attr
+  [attribute]
+  (fn [char] ((comp attribute :attributes) char)))
+; 2. Implement the comp function.
 (defn my-comp
   "Returns a function that takes a number of arguments, and then
   1. takes the last function and applies it to the arguments to get a result.
@@ -13,3 +20,14 @@
     [& args]
     (reduce #(%2 %1) (apply (last functions) args) (rest (reverse functions))))
   )
+; 3. Implement the assoc-in function. Hint: use the assoc function and define its parameters as [m [k & ks] v] .
+(defn my-assoc-in
+  ""
+  ([m [k & ks] v]
+   (if ks
+     (let [new-value (my-assoc-in (get m k) ks v)]
+       (assoc m k new-value))
+     (assoc m k v)))
+  )
+; 4. Look up and use the update-in function.
+; 5. Implement update-in .
--- test/clojure_for_the_brave_and_true/chapter5_test.clj
+++ test/clojure_for_the_brave_and_true/chapter5_test.clj
@@ -7,3 +7,11 @@
   (testing "my-comp"
     (is (= ((my-comp inc) 1) 2) "single function")
     (is (= ((my-comp inc inc +) 1 2) 5) "multiple functions")))
+
+(deftest attr-test
+  (testing "attr"
+    (is (= ((attr :intelligence) {:attributes {:intelligence 10}}) 10))))
+
+(deftest my-assoc-in-test
+  (testing "my-assoc-in"
+    (is (= (my-assoc-in {} [:a :b :c] "friend") {:a {:b {:c "friend"}}}))))
